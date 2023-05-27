f009509e5d2db10a935757abd86c1da198d14900
Allison Blumenthal <blumen.allison@gmail.com>
2023-05-27 12:33:41 -0400

    refactored  game card to pass gameObj, use router, and  include  edit button

--- components/game/gameCard.js
+++ components/game/gameCard.js
@@ -1,28 +1,41 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 
-const GameCard = ({
-  title,
-  maker,
-  numberOfPlayers,
-  skillLevel,
-}) => (
-  <Card className="text-center">
-    <Card.Header>{title}</Card.Header>
-    <Card.Body>
-      <Card.Title>By: {maker}</Card.Title>
-      <Card.Text>{numberOfPlayers} players needed</Card.Text>
-    </Card.Body>
-    <Card.Footer className="text-muted">Skill Level: {skillLevel}</Card.Footer>
-  </Card>
-);
+function GameCard({ gameObj }) {
+  const router = useRouter();
+
+  console.warn(gameObj);
+
+  return (
+    <>
+      <Card className="text-center">
+        <Card.Header>{gameObj.title}</Card.Header>
+        <Card.Body>
+          <Card.Title>By: {gameObj.maker}</Card.Title>
+          <Card.Text>{gameObj.numberOfPlayers} players needed</Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">Skill Level: {gameObj.skillLevel}</Card.Footer>
+        <Button
+          onClick={() => {
+            router.push(`/games/edit/${gameObj.id}`);
+          }}
+        >Edit Game
+        </Button>
+      </Card>
+    </>
+  );
+}
 
 GameCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  maker: PropTypes.string.isRequired,
-  numberOfPlayers: PropTypes.number.isRequired,
-  skillLevel: PropTypes.number.isRequired,
+  gameObj: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    maker: PropTypes.string.isRequired,
+    numberOfPlayers: PropTypes.number.isRequired,
+    skillLevel: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default GameCard;
